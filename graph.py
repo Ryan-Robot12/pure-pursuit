b023a368f8b449244f6ab3ac44341aefeb5bc090
Ryan-Robot12 <ratack0@gmail.com>
2025-10-08 11:02:29 -0400

    debugging stuff (graph.py, data.json)
    Signed-off-by: Ryan-Robot12 <ratack0@gmail.com>

---
+++ graph.py
@@ -0,0 +1,23 @@
+import json
+
+import matplotlib.pyplot as plt
+
+with open("data.json") as file:
+    data = json.load(file)
+
+"""
+"current_pos": self.current_pos,
+"target_pos": target_loc,
+"drive_err": dist,
+"turn_err": desired_heading - self.current_heading,
+"desired_heading": desired_heading,
+"current_heading": self.current_heading,
+"drive_out": drive_output,
+"turn_out": turn_output,
+"dt": time.time() - self._last_time
+"""
+x = [i for i in range(1, len(data["drive_err"]) + 1)]
+
+plt.scatter(x, data["drive_err"])
+plt.scatter(x, data["turn_err"])
+plt.savefig("graph.png")
